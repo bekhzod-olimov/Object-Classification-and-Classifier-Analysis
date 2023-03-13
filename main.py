4ab1a832776bd12fcba87201d7ec3693874c4d36
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-13 09:51:50 +0900

    main script

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ def run(args):
     """
     
     # Get train arguments    
-    argstr = yaml.dump(args.__dict__, default_flow_style=False)
+    argstr = yaml.dump(args.__dict__, default_flow_style = False)
     print(f"\nTraining Arguments:\n{argstr}")
     
     # Get train and validation transformations 
-    train_transformations, valid_transformations= get_transforms(train=True), get_transforms(train=False)
+    train_transformations, valid_transformations= get_transforms(train = True), get_transforms(train = False)
     
     # Get class names, number of classes, train and validation dataloaders
     cls_names, num_classes, tr_dl, val_dl = get_dl(args.root, args.batch_size, valid_transformations)
     print(f"Number of classes in the dataset: {num_classes}\n")
     
     # Initialize model, loss_function, and optimizer    
-    model = timm.create_model(args.model_name, pretrained=True, num_classes=num_classes)
+    model = timm.create_model(args.model_name, pretrained = True, num_classes = num_classes)
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+    optimizer = torch.optim.AdamW(model.parameters(), lr = args.learning_rate)
     
     # Set initial best accuracy
     best_accuracy = 0.
