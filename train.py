ddf0d4a417b5c26e6c9373d99a178dba4649f6d1
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-24 11:30:36 +0900

    train and validation scripts

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 # Import libraries
-import timm, torch
+import timm, torch, os
 from tqdm import tqdm
 
-def saveModel(model):
+def saveModel(model, save_path, best_accuracy, epoch):
     
     '''
     
@@ -15,12 +15,13 @@ def saveModel(model):
     '''
     
     # Set the path to save the trained model
-    path = "./best_model.pth"
+    os.makedirs(f"{save_path}", exist_ok = True)
+    path = f"{save_path}/best_model_{epoch}_{best_accuracy:.1f}.pth"
     
     # Save the model
     torch.save(model.state_dict(), path)
 
-def validation(model, val_dl, device):
+def validation(model, criterion, val_dl, device):
     
     '''
     
@@ -42,7 +43,7 @@ def validation(model, val_dl, device):
     model.eval()
     
     # Set the accuracy and total to 0
-    accuracy, total = 0, 0
+    running_loss, running_acc, total = 0, 0, 0
 
     # Conduct validation without gradients
     with torch.no_grad(): 
@@ -56,20 +57,24 @@ def validation(model, val_dl, device):
             images, labels = images.to(device), labels.to(device)
             # Get the model predictions
             outputs = model(images)
+            
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() 
+            
             # Select the prediction with the highest value
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             
             # Get the accuracy
-            accuracy += (predicted == labels).sum().item()
+            running_acc += (predicted == labels).sum().item()
     
     # Compute the accuracy over all test images
-    accuracy = (100 * accuracy / total)
-    print(f"Validation accuracy is: {accuracy:.3f}")
+    accuracy = (100 * running_acc / total)
+    loss = (running_loss / total)
     
-    return accuracy
+    return loss, accuracy
     
-def train(model, tr_dl, val_dl, num_classes, criterion, optimizer, device, epochs, best_accuracy):
+def train(model, tr_dl, val_dl, num_classes, criterion, optimizer, device, epochs, best_accuracy, save_path):
     
     '''
     
@@ -93,6 +98,8 @@ def train(model, tr_dl, val_dl, num_classes, criterion, optimizer, device, epoch
     # Define your execution device
     print(f"The model will be running on {device} device\n")
     
+    tr_loss, val_loss, tr_accs, val_accs = [], [], [], []
+    
     # Move the model to gpu
     model.to(device)
     
@@ -100,7 +107,7 @@ def train(model, tr_dl, val_dl, num_classes, criterion, optimizer, device, epoch
     for epoch in range(epochs): 
         
         # Set running loss and accuracy
-        running_loss, running_acc = 0, 0
+        running_loss, running_acc, total = 0, 0, 0
         
         # Get through the training dataloader
         for i, batch in tqdm(enumerate(tr_dl, 0)):
@@ -108,26 +115,38 @@ def train(model, tr_dl, val_dl, num_classes, criterion, optimizer, device, epoch
             # Get the inputs and move them to device
             images, labels = batch
             images, labels = images.to(device), labels.to(device)
+            total += labels.shape[0]
 
             # Zero the parameter gradients
             optimizer.zero_grad()
             # Predict classes using images from the training dataloader
             outputs = model(images)
+            
+            _, predicted = torch.max(outputs.data, 1)
             # Compute the loss based on model output and real labels
             loss = criterion(outputs, labels)
+            running_loss += loss.item()     # extract the loss value
+            # Get the accuracy
+            running_acc += (predicted == labels).sum().item()
             # Backpropagate the loss
             loss.backward()
             # Adjust parameters based on the calculated gradients
             optimizer.step()
-            running_loss += loss.item()     # extract the loss value
 
+        tr_loss.append(running_loss / total)
+        tr_accs.append(running_acc / total)
         # Perform validation and get the accuracy score
-        accuracy = validation(model, val_dl, device)
-        print(f"For epoch {epoch+1} the validation accuracy over the whole validation set is {accuracy:.2f}%")
+        val_loss_, val_accuracy = validation(model, criterion, val_dl, device)
+        print(f"\nValidation accuracy on epoch {epoch+1} is {val_accuracy:.3f}%")
+        print(f"Validation loss on epoch {epoch+1} is {val_loss_:.3f}\n")
+        val_loss.append(val_loss_)
+        val_accs.append(val_accuracy)
         
         # Save the model with the best accuracy
-        print(f"The best validation accuracy on epoch {epoch+1} is {best_accuracy:.2f}%")
-        if accuracy > best_accuracy:
-            saveModel(model)
-            best_accuracy = accuracy
+        print(f"The best validation accuracy on epoch {epoch} is {best_accuracy:.2f}%")
+        if val_accuracy > best_accuracy:
+            best_accuracy = val_accuracy
+            saveModel(model, save_path, best_accuracy, epoch+1)
             print(f"The best validation accuracy on epoch {epoch+1} is {best_accuracy:.2f}%")
+            
+    return {"tr_loss": tr_loss, "tr_accs": tr_accs, "val_loss": val_loss, "val_accs": val_accs}
